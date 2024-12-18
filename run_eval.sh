432242b7089a813deb0f76ebd56f4610f3b82b00
Adriana Hugessen <hugessen.adriana@gmail.com>
2024-12-17 23:08:58 -0500

    update readme

--- run_eval.sh
+++ run_eval.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export DISPLAY=:1
 Xvfb :1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset &> /scratch/xvfb.log & 
-for exp in rl_velocity; do
+for exp in partial_irl full_irl rl_huamn rl_velocity; do
     for seed in 0; do
         python scripts/evaluate_policy.py outdir=/workspaces/eval_results/${exp}/${seed} policy_path=/workspaces/policies/${exp}/${seed}/snapshot.pt || continue
     done
